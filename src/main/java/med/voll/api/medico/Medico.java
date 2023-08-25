858ab5efedbea1ab3e32edaf8593bf4346e2d71a
fabiolopes <lbrito.fabio@gmail.com>
2023-08-24 22:06:01 -0300

    feat:new feature atualizar medicos

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -22,8 +22,6 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
     }
 
-
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
@@ -34,5 +32,18 @@ public class Medico {
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 
 }
